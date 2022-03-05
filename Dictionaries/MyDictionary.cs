821ebff37320c3384bf773df76db1494bb3e32b4
Kerem <kerem.kurkcu@hotmail.com>
2022-03-05 15:45:10 +0300

    Add project files.

---
+++ Dictionaries/MyDictionary.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionaries
+{
+    class MyDictionary<Tkey, Tvalue>
+    {
+        Tkey[] dictionaryKey;
+        Tvalue[] dictionaryValue;
+
+        public MyDictionary()
+        {
+            dictionaryKey = new Tkey[0];
+            dictionaryValue = new Tvalue[0];
+        }
+
+        public void Add(Tkey key, Tvalue value)
+        {
+            Tkey[] tempKey = dictionaryKey;
+            Tvalue[] tempValue = dictionaryValue;
+
+            dictionaryKey = new Tkey[dictionaryKey.Length + 1];
+            dictionaryValue = new Tvalue[dictionaryValue.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                dictionaryKey[i] = tempKey[i];
+                dictionaryValue[i] = tempValue[i];
+            }
+
+            dictionaryKey[dictionaryKey.Length - 1] = key;
+            dictionaryValue[dictionaryValue.Length - 1] = value;
+        }
+
+        public void Write()
+        {
+            for (int i = 0; i < dictionaryKey.Length; i++)
+            {
+                Console.WriteLine("Key : " + dictionaryKey[i] + "= " + "Değer: " + dictionaryValue[i]);
+            }
+        }
+
+    }
+}
---
+++ Dictionaries/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Dictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> dictionary = new MyDictionary<string, int>();
+
+            dictionary.Add("Fred ", 22);
+            dictionary.Add("Erik ", 36);
+            dictionary.Add("Elise ", 18);
+            dictionary.Add("Beth ", 29);
+            dictionary.Write();
+            Console.ReadLine();
+        }
+    }
+}
